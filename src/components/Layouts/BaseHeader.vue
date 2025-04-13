cee86bf702b7a8d195497bd13ecff9bfa546b648
wherewhale <syh980130@gmail.com>
2025-04-14 00:25:13 +0900

    feat: add main feature

---
+++ src/components/Layouts/BaseHeader.vue
@@ -0,0 +1,15 @@
+<style scoped>
+header {
+  padding: 1.25rem 1rem;
+}
+</style>
+
+<template>
+  <header class="bg-white">
+    <Typography tag="h2" color="black" weight="bold" size="lg" class="mb-0"> 정처기 실기 개념 </Typography>
+  </header>
+</template>
+
+<script setup>
+import Typography from '../Common/Typography.vue';
+</script>
---
+++ src/components/Layouts/BaseLayout.vue
@@ -0,0 +1,25 @@
+<style scoped>
+.container {
+  max-width: 768px !important;
+  /* TODO: 개발 이후 border 삭제 */
+  border: 1px solid #000;
+  min-height: 100vh;
+  padding: 0;
+  position: relative;
+}
+main {
+  padding: 2rem 1rem 6rem;
+}
+</style>
+<template>
+  <div class="container bg-gray-1">
+    <BaseHeader />
+    <main class="d-flex align-items-center flex-column">
+      <slot></slot>
+    </main>
+  </div>
+</template>
+
+<script setup>
+import BaseHeader from './BaseHeader.vue';
+</script>
