a9e8cb4b447479c9897f052ff61417d0a0cdd519
wherewhale <syh980130@gmail.com>
2025-04-15 00:35:36 +0900

    feat: add ga4

--- src/main.js
+++ src/main.js
@@ -5,8 +5,15 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 
+import { createGtag } from 'vue-gtag';
+
 const app = createApp(App);
 
+const gtag = createGtag({
+  tagId: import.meta.env.VITE_GA_ID,
+});
+
 app.use(router);
+app.use(gtag);
 
 app.mount('#app');
