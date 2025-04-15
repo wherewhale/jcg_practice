ab65bf5ec8e007e12e173cfe4555d69f3f581615
wha1e <syh980130@gmail.com>
2025-04-15 15:52:16 +0900

    feat(ads): add ads tag

--- src/components/Layouts/BaseFooter.vue
+++ src/components/Layouts/BaseFooter.vue
@@ -1,7 +1,24 @@
-<style scoped></style>
+<style scoped>
+.ads-wrapper {
+  max-width: 100%;
+  margin: 1rem auto;
+  display: flex;
+  justify-content: center;
+}
+</style>
 
 <template>
-  <footer class="d-flex justify-content-center text-center">
+  <footer class="d-flex flex-column justify-content-center text-center">
+    <div class="ads-wrapper">
+      <ins
+        class="adsbygoogle"
+        style="display: block"
+        data-ad-client="ca-pub-4940966521130158"
+        data-ad-slot="YYYYYYY"
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      ></ins>
+    </div>
     <Typography color="gray" size="md" weight="regular">
       © 2025. Wha1eSon<br />
       all rights reserved.
@@ -10,5 +27,5 @@
 </template>
 
 <script setup>
-import Typography from '../Common/Typography.vue';
+import Typography from '../Common/Typography.vue'
 </script>
